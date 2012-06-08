3ecde71db2803350602755b87a3fabe7f6165b0f
Roman Pominov <meshokmuki@gmail.com>
2012-06-08 18:00:30 +0400

    Зачатки депллоя

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
-# ncftpput -R -S .tmp -v -u thecand8 -p *** ftp.thecandies.ru /www ./site_root/*
+#!/bin/sh
+
+HOST=ftp.thecandies.ru
+LOGIN=thecand8
+PASSWORD=$1
+
+ncftpput -u $LOGIN -p $PASSWORD -d deploy-ftp.log -R -S .tmp -v $HOST /www/published/SC/html/scripts/core_functions ./site_root/published/SC/html/scripts/core_functions/registration_functions.php
+ncftpput -u $LOGIN -p $PASSWORD -d deploy-ftp.log -R -S .tmp -v $HOST /www/published/SC/html/scripts/modules/news ./site_root/published/SC/html/scripts/modules/news/class.newsmodule.php
 
