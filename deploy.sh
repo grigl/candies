7962f141cdb8ba08e308bc7757c68f6c0976f3b9
Roman Pominov <meshokmuki@gmail.com>
2012-06-11 23:29:12 +0400

    Ого какой я рубист

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-HOST=ftp.thecandies.ru
-LOGIN=thecand8
-PASSWORD=$1
-
-ncftpput -u $LOGIN -p $PASSWORD -d deploy-ftp.log -R -S .tmp -v $HOST /www/published/SC/html/scripts/core_functions ./site_root/published/SC/html/scripts/core_functions/registration_functions.php
-ncftpput -u $LOGIN -p $PASSWORD -d deploy-ftp.log -R -S .tmp -v $HOST /www/published/SC/html/scripts/modules/news ./site_root/published/SC/html/scripts/modules/news/class.newsmodule.php
-
