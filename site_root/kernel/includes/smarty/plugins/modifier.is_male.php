9a7c1ffd32d2956e5a0aec3a9ed7639f11460710
Roman Pominov <meshokmuki@gmail.com>
2012-06-13 14:00:43 +0400

    М/Ж перевороты

---
+++ site_root/kernel/includes/smarty/plugins/modifier.is_male.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * Type:     modifier
+ * Name:     is_male
+ * Purpose:  Returns true if product with given $category_id is male
+ *
+ * Принимаем категорию и проверяем правда ли что ее родительская категория имеет slug «m» 
+ * Подразумевается только 1 уровень вложенности!
+ *
+ * -------------------------------------------------------------
+ */
+function smarty_modifier_is_male($category_id)
+{
+	return "m" == db_phquery_fetch(DBRFETCH_FIRST, 'SELECT a.slug FROM ?#CATEGORIES_TABLE as a, ?#CATEGORIES_TABLE as b WHERE a.categoryID = b.parent AND b.categoryID=?', $category_id);
+}
