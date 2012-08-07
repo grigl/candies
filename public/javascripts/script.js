272f4aac9e716e186ef83705f6f02997f39374d0
Roman Pominov <meshokmuki@gmail.com>
2012-08-07 20:42:51 +0400

    Аякс-валидация

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -87,6 +87,36 @@ $(document).bind('html-inserted', function() {
 
 });
 
+
+// Аякс-валидация
+// см. также update_errors.js.erb
+$(document).on('ajax:error', 'form', function(__1, __2, __3, obj) {
+	if (obj && obj.type === "candies-form-validation") {
+		var errors = obj.errors;
+		for (var name in errors) {
+			var error_text = errors[name][0];
+			name = name.split('.');
+			// нужно добавить _attributes ко всем частям кроме последней
+			for (var i = 0; i < name.length - 1; i++) {
+				name[i] += '_attributes';
+			}
+			$('[name$="[' + name.join('][') + ']"]', this)
+				.addClass('error')
+				.parent()
+				.prepend('<div class="errorlist">' + error_text + '</div>');
+		}
+	}
+});
+$(document).on('ajax:beforeSend', 'form', function() {
+	$('.error[name]', this).removeClass('error');
+	$('.errorlist', this).remove();
+});
+
+
+
+
+
+
 // placeholder
 (function(b){function d(a){this.input=a;a.attr("type")=="password"&&this.handlePassword();b(a[0].form).submit(function(){if(a.hasClass("placeholder")&&a[0].value==a.attr("placeholder"))a[0].value=""})}d.prototype={show:function(a){if(this.input[0].value===""||a&&this.valueIsPlaceholder()){if(this.isPassword)try{this.input[0].setAttribute("type","text")}catch(b){this.input.before(this.fakePassword.show()).hide()}this.input.addClass("placeholder");this.input[0].value=this.input.attr("placeholder")}},
 hide:function(){if(this.valueIsPlaceholder()&&this.input.hasClass("placeholder")&&(this.input.removeClass("placeholder"),this.input[0].value="",this.isPassword)){try{this.input[0].setAttribute("type","password")}catch(a){}this.input.show();this.input[0].focus()}},valueIsPlaceholder:function(){return this.input[0].value==this.input.attr("placeholder")},handlePassword:function(){var a=this.input;a.attr("realType","password");this.isPassword=!0;if(b.browser.msie&&a[0].outerHTML){var c=b(a[0].outerHTML.replace(/type=(['"])?password\1/gi,
