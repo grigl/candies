e63af067f2f46422fa39817d271d89e02feab26f
Gray Hound <admin@devilmaydie.name>
2012-08-08 19:00:22 +0600

    реализован вывод "все коллекции"

--- app/controllers/taxons_controller.rb
+++ app/controllers/taxons_controller.rb
@@ -5,19 +5,26 @@ class TaxonsController < Spree::BaseController
   respond_to :html
 
   def show
+    all_collection_id = 5 #грязный грязный хак
+    
     @taxon = Taxon.find_by_permalink!(params[:id])
     return unless @taxon
 
-    @searcher = Spree::Config.searcher_class.new(params.merge(:taxon => @taxon.id))
-    all_products = @searcher.retrieve_products
-
+    if @taxon.id == all_collection_id then
+      all_products = Product.all
+    else
+      @searcher = Spree::Config.searcher_class.new(params.merge(:taxon => @taxon.id)) 
+      all_products = @searcher.retrieve_products 
+    end
+    
     all_products_by_gender = {"male" => [], "female" => []}
     all_products.each do|product|
+      puts product.gender
       if product.gender == 0 or product.gender == 1 then
-        all_products_by_gender["male"].push(product)
+        all_products_by_gender["male"].push product
       end
       if product.gender == 0 or product.gender == 2 then
-        all_products_by_gender["female"].push(product)
+        all_products_by_gender["female"].push product
       end
     end
 
