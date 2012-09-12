366f4257a2392180f6da1a65f3e308038723cfe9
Andrey Karandin <karandin72@gmail.com>
2012-09-12 20:49:51 +0600

    script.js synt fix

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -228,7 +228,7 @@ function transformVariantChooser() {
 			for (size in colors[color]) {
 				size_select.append('<option value="' + size + '" data-value="' + colors[color][size] + '">' + size + '</option>');
 			}
-			var sortedVals = $.makeArray($(size_select +' option')).sort(function(a,b){
+			var sortedVals = $.makeArray($(size_select).find('option')).sort(function(a,b){
 				return parseInt($(a).text()) > parseInt($(b).text()) ? 1 : parseInt($(a).text()) < parseInt($(b).text()) ? -1 : 0 ;
 			});
 			size_select.empty().html(sortedVals);
