7778b1cdf9ae51118cf07ea1bba091a9f39d9708
busyspirit <spirit.tmn@gmail.com>
2012-09-13 15:46:07 +0600

    upd styles.css

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -278,6 +278,8 @@ $(document).bind('html-inserted', function() {
 		ddFullWidth: false
 	});
 	
+	$('.ik_select_option').attr('title', '');
+	
 	$('.select.f-hide').each(function(){
 		$(this).parent().find('.ik_select_link_text').empty();
 	});
