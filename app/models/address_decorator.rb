1619874447471e88d1165bccc81ffbd48b7ee4f9
gleb grishin <griglmail@gmail.com>
2012-10-12 12:34:48 +0400

    фикс копирования адресов

--- app/models/address_decorator.rb
+++ app/models/address_decorator.rb
@@ -9,7 +9,7 @@ Address.class_eval do
 
   def same_as_without_contacts?(other)
     return false if other.nil?
-    attributes.except("id", "updated_at", "address2", "created_at", "default", 'firstname', 'lastname', 'secondname', 'phone', 'user_id') ==  other.attributes.except("id", "updated_at", "created_at", "address2", "default", 'firstname', 'lastname', 'secondname', 'phone', 'user_id')
+    attributes.except("id", "updated_at", "created_at", "default", 'firstname', 'lastname', 'phone', 'user_id') ==  other.attributes.except("id", "updated_at", "created_at", "default", 'firstname', 'lastname', 'phone', 'user_id')
   end
 
   def to_s
